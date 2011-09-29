426227ce131dd788eaaa2f570094942cc6ef701a
Tobias Crawley <tcrawley@gmail.com>
2011-09-29 13:19:30 -0400

    Move RackApplicationDefaultsProcessor up, since it really applies to all web apps.

---
+++ web/src/main/java/org/projectodd/polyglot/web/processors/WebApplicationDefaultsProcessor.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2008-2011 Red Hat, Inc, and individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.projectodd.polyglot.web.processors;
+
+import org.jboss.as.server.deployment.DeploymentPhaseContext;
+import org.jboss.as.server.deployment.DeploymentUnit;
+import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
+import org.jboss.as.server.deployment.DeploymentUnitProcessor;
+import org.projectodd.polyglot.web.WebApplicationMetaData;
+
+public class WebApplicationDefaultsProcessor implements DeploymentUnitProcessor {
+
+    public static final String DEFAULT_HOST = "localhost";
+    public static final String DEFAULT_CONTEXT_PATH = "/";
+    public static final String DEFAULT_STATIC_PATH_PREFIX = "public/";
+
+    public WebApplicationDefaultsProcessor() {
+    }
+
+    @Override
+    public void deploy(DeploymentPhaseContext phaseContext) throws DeploymentUnitProcessingException {
+        DeploymentUnit unit = phaseContext.getDeploymentUnit();
+        
+        WebApplicationMetaData metadata = unit.getAttachment( WebApplicationMetaData.ATTACHMENT_KEY );
+        
+        if ( metadata == null ) {
+            return;
+        }
+        
+        if (metadata.getHosts().isEmpty()) {
+            metadata.addHost( DEFAULT_HOST );
+        }
+
+        if ((metadata.getContextPath() == null) || (metadata.getContextPath().trim().equals( "" ))) {
+            metadata.setContextPath( DEFAULT_CONTEXT_PATH );
+        }
+        
+        if ((metadata.getStaticPathPrefix() == null) || (metadata.getStaticPathPrefix().trim().equals( "" ))) {
+            metadata.setStaticPathPrefix( DEFAULT_STATIC_PATH_PREFIX );
+        }
+    }
+
+    @Override
+    public void undeploy(DeploymentUnit context) {
+    }
+    
+}
