c29ad29936fbd447bbff4f04925f662a91f01221
Tobias Crawley <tcrawley@gmail.com>
2012-01-16 12:38:55 -0500

    Pull generic runtime installation up from immutant.

---
+++ core-extensions/src/main/java/org/projectodd/polyglot/core_extensions/AtRuntimeInstaller.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2008-2012 Red Hat, Inc, and individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+ 
+package org.projectodd.polyglot.core_extensions;
+
+import java.util.Hashtable;
+
+import javax.management.MBeanServer;
+
+import org.jboss.as.jmx.MBeanRegistrationService;
+import org.jboss.as.jmx.MBeanServerService;
+import org.jboss.as.jmx.ObjectNameFactory;
+import org.jboss.as.server.deployment.DeploymentUnit;
+import org.jboss.msc.service.Service;
+import org.jboss.msc.service.ServiceBuilder;
+import org.jboss.msc.service.ServiceBuilder.DependencyType;
+import org.jboss.msc.service.ServiceController.Mode;
+import org.jboss.msc.service.ServiceName;
+import org.jboss.msc.service.ServiceTarget;
+import org.jboss.msc.service.StartContext;
+import org.jboss.msc.service.StartException;
+import org.jboss.msc.service.StopContext;
+import org.projectodd.polyglot.core.app.ApplicationMetaData;
+import org.projectodd.polyglot.hasingleton.HASingleton;
+
+public class AtRuntimeInstaller<T> implements Service<T>  {
+
+    public AtRuntimeInstaller(DeploymentUnit unit) {
+        this.unit = unit;
+    }
+    
+    protected void deploy(final ServiceName serviceName, Service<?> service, boolean singleton) {
+        ServiceBuilder<?> builder = this.serviceTarget.addService(serviceName, service);
+        if (singleton) {
+            builder.addDependency(HASingleton.serviceName(unit));
+            builder.setInitialMode(Mode.PASSIVE);
+        } else {
+            builder.setInitialMode(Mode.ACTIVE);
+        }
+        
+        builder.install();  
+    }
+    
+    protected void installMBean(final ServiceName name, MBeanRegistrationService<?> mbeanService) {
+        this.serviceTarget.addService( name.append( "mbean" ), mbeanService )
+            .addDependency( DependencyType.OPTIONAL, MBeanServerService.SERVICE_NAME, MBeanServer.class, mbeanService.getMBeanServerInjector() )
+            .setInitialMode( Mode.PASSIVE )
+            .install(); 
+    }
+    
+    protected String mbeanName(final String domain, final ServiceName name) {
+        final ApplicationMetaData appMetaData = unit.getAttachment( ApplicationMetaData.ATTACHMENT_KEY );
+        return ObjectNameFactory.create( domain, new Hashtable<String, String>() {
+            {
+                put( "app", appMetaData.getApplicationName() );
+                put( "name", name.getSimpleName() );
+            }
+        } ).toString();    
+    }
+    
+    @Override
+    public void start(StartContext context) throws StartException {
+        this.serviceTarget = context.getChildTarget();
+    }
+    
+    @Override
+    public synchronized void stop(StopContext context) {
+    }
+    
+    @Override
+    public T getValue() {
+        return (T)this;
+    }
+    
+    public DeploymentUnit getUnit() {
+        return unit;
+    }
+
+    private DeploymentUnit unit;
+    private ServiceTarget serviceTarget;
+
+}
